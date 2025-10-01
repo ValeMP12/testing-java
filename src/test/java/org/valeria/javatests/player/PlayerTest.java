86a3a4f157f1f3f7705f3e97bda2839d31043e05
ValeMP12 <preciadovaleriam@gmail.com>
2025-10-01 14:07:56 -0600

    Simulacion de clases con Moquito

---
+++ src/test/java/org/valeria/javatests/player/PlayerTest.java
@@ -0,0 +1,23 @@
+package org.valeria.javatests.player;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @Test
+    public void looseWhenDiceNumberIsTooLow() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(2);
+        Player player = new Player(dice, 3);
+        assertFalse(player.play());
+    }
+    @Test
+    public void winsWhenDiceNumberIsBig() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(4);
+        Player player = new Player(dice, 3);
+        assertTrue(player.play());
+    }
+
+}
