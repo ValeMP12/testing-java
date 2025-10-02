09c051008389bc65bedbaa646f529045e4af0f29
ValeMP12 <preciadovaleriam@gmail.com>
2025-10-02 12:45:42 -0600

    TDD para calcular años bisiestos

--- src/test/java/org/valeria/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/org/valeria/javatests/payments/PaymentProcessorTest.java
@@ -1,27 +1,34 @@
 package org.valeria.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PaymentProcessorTest {
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setUp() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+
+
+    }
 
     @Test
     public void paymentIsCorrect() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                .thenReturn(new PaymentResponse(PaymentResponse.PaymentSatus.OK));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertTrue(paymentProcessor.makePayment(1000));
     }
     @Test
     public void paymentIsWrong() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentSatus.ERROR));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
     }
---
+++ src/test/java/org/valeria/javatests/util/DateUtilLeapYearTest.java
@@ -0,0 +1,38 @@
+package org.valeria.javatests.util;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DateUtilLeapYearTest {
+    @Test
+    public void returnTrueWhenYearIsDivisibleBy400() {
+        assertThat(DateUtil.isLeapYear(1600), is(true));
+        assertThat(DateUtil.isLeapYear(2000), is(true));
+        assertThat(DateUtil.isLeapYear(2400), is(true));
+    }
+    @Test
+    public void returnFalseWhenYearIsDivisibleBy100ButNotBy400() {
+        assertThat(DateUtil.isLeapYear(1700), is(false));
+        assertThat(DateUtil.isLeapYear(1800), is(false));
+        assertThat(DateUtil.isLeapYear(1900), is(false));
+    }
+
+    @Test
+    public void returnTrueWhenYearIsDivisibleBy4ButNotBy100() {
+        assertThat(DateUtil.isLeapYear(1996), is(true));
+        assertThat(DateUtil.isLeapYear(2004), is(true));
+        assertThat(DateUtil.isLeapYear(2008), is(true));
+    }
+    @Test
+    public void returnFalseWhenYearIsDivisibleBy4() {
+        assertThat(DateUtil.isLeapYear(2017), is(false));
+        assertThat(DateUtil.isLeapYear(2018), is(false));
+        assertThat(DateUtil.isLeapYear(2019), is(false));
+    }
+}
